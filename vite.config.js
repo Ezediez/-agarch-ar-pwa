8fd7b609494b3fcbf7fcc3357eb6fdf6e0a4a8f3
Ezediez <ezequieldiez@hotmail.com>
2025-09-20 21:17:47 -0300

    Update vite config and build files with unique naming

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,9 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
         manualChunks: {
           vendor: ['react', 'react-dom'],
           firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage']
